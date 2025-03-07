53ee3a0f272f6c8886105d82816fca019cf07f88
Andrea <andreacollado99@gmail.com>
2025-03-07 13:03:03 +0100

    Primer Commit desde rama1

--- src/PruebasEGit/PruebasEGit.java
+++ src/PruebasEGit/PruebasEGit.java
@@ -5,6 +5,7 @@ public class PruebasEGit {
 	public static void main(String[] args) {
 		System.out.println("Pruebas EGit");
 		System.out.println("Primer cambio");
+		System.out.println("Cambio desde rama1");
 	}
 
 }
